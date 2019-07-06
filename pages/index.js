cfb0d173cce82cb91cf0281b6e63a8389859de1a
Marcel Ashun <marcel.ashun@gmail.com>
2019-07-06 15:10:39 +0200

    Textbreite angepasst
    max-width der Textblöcke vergrößert.

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Intro = styled.p`
     max-width: 75%;
   }
   @media screen and (min-width: 1280px) {
-    max-width: 55%;
+    max-width: 75%;
   }
   @media screen and (min-width: 1400px) {
     max-width: 50%;
