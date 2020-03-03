e0e1edb773eed52ad7c22980e88107cc09e045c1
Marcel Ashun <marcel.ashun@gmail.com>
2020-03-03 17:11:24 +0100

    Navbar E-Mail hinzugefügt

--- components/social_nav.js
+++ components/social_nav.js
@@ -13,6 +13,9 @@ export default ({ children }) => (
       }
       @media screen and (min-width: 1120px) {
         .social-nav { top: 24px; }
+        .navbar_divider {
+          top: 6px;
+        }
       }
       a[href^="tel"] {
         color: white;
@@ -35,10 +38,49 @@ export default ({ children }) => (
         position: relative;
         text-decoration: none;
       }
-      .navbar_phonenumber:hover {
+      .navbar_email {
+        background-image: url("../static/icon-email.svg");
+        background-position: 0 1px;
+        background-repeat: no-repeat;
+        background-size: 16px;
+        bottom: -14px;
+        color: white;
+        display: inline-block;
+        font-family: 'Archivo Narrow',sans-serif;
+        font-size: 14px;
+        font-weight: light;
+        height: 30px;
+        margin: 0 16px 0 0;
+        padding: 0 0 0 18px;
+        position: relative;
+        text-decoration: none;
+      }
+      .navbar_divider {
+        width: 2px;
+        height: 22px;
+        background-color: #fff;
+        position: absolute;
+        top: 10px;
+        left: 26px;
+      }
+      .navbar_phonenumber:hover,
+      .navbar_email:hover {
         opacity: 0.8;
       }
+
       @media screen and (min-width: 580px) {
+        .navbar_email {
+          background-size: 24px;
+          font-size: 20px;
+          padding: 0 0 0 26px;
+          margin: 0 16px 0 0;
+          bottom: -9px;
+        }
+        .navbar_divider {
+          height: 30px;
+          top: 4px;
+          left: 35px;
+        }
         .navbar_phonenumber {
           background-size: 24px;
           font-size: 20px;
@@ -58,16 +100,27 @@ export default ({ children }) => (
           padding-right: 10px;
         }
       }
-      @media screen and (min-width: 1060px) {
+      @media screen and (min-width: 1147px) {
         .icon {
           height: 32px;
           padding-left: 15px;
           padding-right: 15px;
         }
       }
+      @media screen and (min-width: 1120px) {
+        .navbar_email {
+          margin: 0 21px 0 0;
+        }
+        .navbar_divider {
+          left: 37px;
+          top: 6px;
+        }
+      }
 
     `}</style>
 
+    <a className="navbar_email" href='mailto:info@gedankenwerk.com'></a>
+    <div className="navbar_divider"></div>
     <a className="navbar_phonenumber" href='tel:+4920163019133'>0201 630 191 - 33</a>
     <a rel="noopener" href='https://www.facebook.com/gedankenwerk' target="_blank">
       <img className="icon"src="/static/icon-facebook@2x.png" alt="Gedankenwerk auf Facebook" />
